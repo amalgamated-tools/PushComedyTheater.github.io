8495baac33f7fb04e2abc06f819821b19156739e
Patrick Veverka <patrick.veverka@gmail.com>
2019-03-31 16:17:43 -0400

    filessssss

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -6,13 +6,22 @@ ls
 git status
 cp -R $GITHUB_WORKSPACE/ /tmp
 cd /tmp/workspace
-echo "{}" >stuff.json
-git config --global user.email "bot@veverka.net"
-git config --global user.name "Your Name"
 
+echo "CREATING STUFF"
+echo "{}" >stuff.json
+echo "SETTING GLOBAL"
+
+git config --global user.email "bot@veverka.net"
+git config --global user.name "Patrick Veverka"
+echo "GIT STATUS"
 git status
+echo "GIT ADD"
 git add stuff.json
+echo "GIT STATUS"
 git status
+echo "GIT COMMIT"
 git commit -m 'adding automagit'
+echo "GIT PUSH ORIGIN"
 git push origin master
+echo "GIT STATUS"
 git status
