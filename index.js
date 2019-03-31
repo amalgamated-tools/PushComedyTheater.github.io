2a77ebe7eb22cbe2da2cdaa068cc956fcc9705d2
Patrick Veverka <patrick.veverka@gmail.com>
2019-03-31 15:22:00 -0400

    reverse

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ files.forEach(cssFile => {
 
         let html = fs.readFileSync(element, 'utf8');
 
+
+
+
         const whitelist = /#foo|\.bar/;
 
         let dropped = new Set();
@@ -40,31 +43,21 @@ files.forEach(cssFile => {
 
 
         if (dropped.size > 0) {
-          console.log(" STUFF DROPPED");
-          var oldurl = "https://cdn.pushcomedytheater.com/pushassets/css/" + cssFile + ".css";
-          console.log(oldurl);
-          var newurl = "https://pushcomedytheater.com/css/" + name + "." + cssFile + ".css";
-          console.log(newurl);
-          html = html.replace(oldurl, newurl);
-          fs.writeFileSync(`./css/${name}.${cssFile}.css`, cleaned.css);
-          fs.writeFileSync(element, html);
+          fs.writeFile(`./css/${name}.${cssFile}.css`, cleaned.css, function (err) {
+            if (err) {
+              return console.log(err);
+            }
+          });
         } else {
-          console.log("  NO CHANGES DROPPED");
-          var oldurl = "<link rel=\"stylesheet\" href=\"https://cdn.pushcomedytheater.com/pushassets/css/unify-globals.css\">";
-          console.log(oldurl);
-
-
-          html = html.replace(oldurl, "");
-          fs.writeFileSync(element, html);
+          console.log("NO CHANGES DROPPED");
         }
-
-
-
       };
     }
   });
 
 });
+
+
 
 
 
