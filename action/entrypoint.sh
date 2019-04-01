fb1081469f44388154f561b860db8b91c91d588e
Patrick Veverka <patrick.veverka@gmail.com>
2019-03-31 20:52:02 -0400

    adding

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,29 +1,30 @@
 #!/bin/sh -l
 set -x
 printenv
+git config --global credential.helper cache
+git config --global user.email "bot@veverka.net"
+git config --global user.name "Patrick Veverka"
 
-# git config --global user.email "bot@veverka.net"
-# git config --global user.name "Patrick Veverka"
+ls -al ~/.ssh
+# cd $GITHUB_WORKSPACE
+# ls
+# git status
+# cp -R $GITHUB_WORKSPACE/ /tmp
+# cd /tmp/workspace
 
-cd $GITHUB_WORKSPACE
-ls
-git status
-cp -R $GITHUB_WORKSPACE/ /tmp
-cd /tmp/workspace
+# echo "CREATING STUFF"
+# echo "{}" >stuff.json
+# echo "SETTING GLOBAL"
 
-echo "CREATING STUFF"
-echo "{}" >stuff.json
-echo "SETTING GLOBAL"
-
-echo "GIT STATUS"
-git status
-echo "GIT ADD"
-git add stuff.json
-echo "GIT STATUS"
-git status
-echo "GIT COMMIT"
-git commit --author='Patrick Veverka <bot@veverka.net>' -m 'adding automagit'
-echo "GIT PUSH ORIGIN"
-git push origin master
-echo "GIT STATUS"
-git status
+# echo "GIT STATUS"
+# git status
+# echo "GIT ADD"
+# git add stuff.json
+# echo "GIT STATUS"
+# git status
+# echo "GIT COMMIT"
+# git commit --author='Patrick Veverka <bot@veverka.net>' -m 'adding automagit'
+# echo "GIT PUSH ORIGIN"
+# git push origin master
+# echo "GIT STATUS"
+# git status
