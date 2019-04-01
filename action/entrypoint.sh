91390f1892aa198e85f91080be2a5e33953d32d2
Patrick Veverka <patrick.veverka@gmail.com>
2019-03-31 20:57:05 -0400

    last try

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,32 +1,23 @@
 #!/bin/sh -l
-set -x
 printenv
 
 cd /tmp
+pwd
 
 git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/PushComedyTheater/PushComedyTheater.github.io.git
-ls -al
 cd PushComedyTheater.github.io/
 ls -al
-# cd $GITHUB_WORKSPACE
-# ls
-# git status
-# cp -R $GITHUB_WORKSPACE/ /tmp
-# cd /tmp/workspace
+echo "{}" >boose.json
+echo "GIT STATUS"
+git status
+echo "GIT ADD"
+git add .
 
-# echo "CREATING STUFF"
-# echo "{}" >stuff.json
-# echo "SETTING GLOBAL"
-
-# echo "GIT STATUS"
-# git status
-# echo "GIT ADD"
-# git add stuff.json
-# echo "GIT STATUS"
-# git status
-# echo "GIT COMMIT"
-# git commit --author='Patrick Veverka <bot@veverka.net>' -m 'adding automagit'
-# echo "GIT PUSH ORIGIN"
-# git push origin master
-# echo "GIT STATUS"
-# git status
+echo "GIT STATUS"
+git status
+echo "GIT COMMIT"
+git commit --author='Patrick Veverka <bot@veverka.net>' -m 'adding automagit'
+echo "GIT PUSH ORIGIN"
+git push origin master
+echo "GIT STATUS"
+git status
