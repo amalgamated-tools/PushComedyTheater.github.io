1c96528822f033055ebd96de7878c0e95b660491
Patrick Veverka <patrick.veverka@gmail.com>
2019-03-31 21:25:03 -0400

    super cool

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,23 +1,2 @@
 #!/bin/sh -l
-printenv
-
-cd /tmp
-pwd
-
-git clone https://$GITHUB_TOKEN:x-oauth-basic@github.com/PushComedyTheater/PushComedyTheater.github.io.git
-cd PushComedyTheater.github.io/
-ls -al
-echo "{}" >boose.json
-echo "GIT STATUS"
-git status
-echo "GIT ADD"
-git add .
-
-echo "GIT STATUS"
-git status
-echo "GIT COMMIT"
-git commit --author='Patrick Veverka <bot@veverka.net>' -m 'adding automagit'
-echo "GIT PUSH ORIGIN"
-git push origin master
-echo "GIT STATUS"
-git status
+echo "SUPER COOL"
