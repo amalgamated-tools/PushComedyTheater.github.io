ee6458ad910ce6ba8d84d89afd9ea9cd1f99bbae
Patrick Veverka <patrick.veverka@gmail.com>
2019-04-03 21:18:48 -0400

    entrypoint

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,23 +1,24 @@
-#!/bin/sh -l
-echo "SUPER COOL NOW"
-
 #!/bin/bash
 set -e
 set -o pipefail
 
+echo "SETTING TOKEN"
 if [[ -n "$TOKEN" ]]; then
   GITHUB_TOKEN=$TOKEN
 fi
 
+echo "SETTING PAGES_BRANCH"
 if [[ -n "$PAGES_BRANCH" ]]; then
   PAGES_BRANCH="master"
 fi
 
+echo "SETTING GITHUB_TOKEN"
 if [[ -z "$GITHUB_TOKEN" ]]; then
   echo "Set the GITHUB_TOKEN env variable."
   exit 1
 fi
 
+echo "SETTING REPOSITORY"
 if [[ -z "$GITHUB_REPOSITORY" ]]; then
   echo "Set the GITHUB_REPOSITORY env variable."
   exit 1
@@ -37,8 +38,8 @@ main() {
 
   # cd public
   # git init
-  # git config user.name "GitHub Actions"
-  # git config user.email "github-actions-bot@users.noreply.github.com"
+  git config user.name "GitHub Actions"
+  git config user.email "github-actions-bot@users.noreply.github.com"
   # git add .
 
   # git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
