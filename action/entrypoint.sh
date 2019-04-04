bc432aaa5f4bd8040e959ab8f50e2210d37394ae
Patrick Veverka <patrick.veverka@gmail.com>
2019-04-03 20:59:04 -0400

    try

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh -l
 echo "SUPER COOL NOW"
-curl -X POST https://6919f78d.ngrok.io/ -d ''
+printenv
+remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" &&
+  remote_branch="gh-pages" &&
+  git init &&
+  git config user.name "${GITHUB_ACTOR}" &&
+  git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
